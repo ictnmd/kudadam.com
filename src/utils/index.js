e311937299d7625c4080ac5beb530a9632d7d69a
Lucretius Biah <lucretiusbiah@protonmail.com>
2022-07-25 15:21:49 +0000

    Tried adding types to some of the functions in the utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,15 @@
 import readingTime from './reading-time.js';
 
 export { readingTime };
+
+/** Takes an array and split it into chunks
+ * @param {Array<any>} collection - The array to be splitted
+ * @param {Number} size - The number of values in each chunk
+ * @returns {Array<any>} result Returns a new array splitted in chunks
+ */
 export const chunk = (collection, size) => {
 	var result = [];
-	size = parseInt(size) || 2;
+	size = size || 2;
 
 	for (var x = 0; x < Math.ceil(collection.length / size); x++) {
 		var start = x * size;
@@ -14,12 +20,19 @@ export const chunk = (collection, size) => {
 	return result;
 };
 
-export const snakeCase = (str) =>
+/** Takes a string and converts it into snakecase 
+ * @param {String} str - The string to be converted
+ * @returns {String} 
+ */
+export const snakeCase = (str) => {
+	let snakecase = 
 	str &&
 	str
 		.match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
-		.map((x) => x.toLowerCase())
+		?.map((x) => x.toLowerCase())
 		.join('_');
+	return snakecase || str;
+}
 
 export const importScripts = (url, object) => {
 	return new Promise((resolve, reject) => {
@@ -63,13 +76,24 @@ export const includesMany = (arr, values) => {
 	});
 };
 
+/** Takes a string of HTML and make it safe to be rendered
+ * @param {String} string - The string to be encoded 
+ * @returns {String} the encoded string
+ */
 export const encodeHtml = (string) => {
 	return string.replace(/[\u00A0-\u9999<>\&]/g, function (i) {
 		return '&#' + i.charCodeAt(0) + ';';
 	});
 };
 
+/** A debounce function takes a function and restricts its calling multiple times
+ * @param {Function} func - The function which is to be debounced
+ * @param {Number} wait - The number of milliseconds to wait 
+ * @param {Boolean} immediate - Set to true to immediately execute the function 
+ * @returns 
+ */
 export function debounce(func, wait, immediate) {
+	/**@type {NodeJS.Timeout} */
 	let timeout;
 	return () => {
 		let context = this;
